021bca07734963a5a8cd1758634f2e27b0bb297a
tintintje <christine.vinck@gmail.com>
2015-10-30 11:08:33 +0100

    Initial Commit

---
+++ Papiamento/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Papiamento
+//
+//  Created by Christine Vinck on 30/10/15.
+//  Copyright © 2015 Christine Vinck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
