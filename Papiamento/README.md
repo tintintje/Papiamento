8c894c92c9800a34abff08876cd6a80bbefd4d1c
tintintje <christine.vinck@gmail.com>
2015-10-30 13:10:06 +0100

    made button and label connections to view

---
+++ Papiamento/README.md
@@ -0,0 +1,5 @@
+Christine Vinck <10003897@student.uva.nl>
+
+Papiamento 1.0 - This app translates a few dutch words on the screen and asks
+the user to pick the larger number.  Perfect for Berkeley students!
+Note: Runs best on big Android tablets because of 1000dp font choice.
--- Papiamento/ViewController.swift
+++ Papiamento/ViewController.swift
@@ -10,16 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var papiamento: UILabel!
+    @IBOutlet weak var translated: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        papiamento.text = "Ban papia Papiamento!"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func translation(sender: UIButton) {
+        
+        let word = sender.currentTitle!
+        
+        switch word {
+            case "Hallo!": translate("Halo!")
+            case "Hoe gaat het?": translate("Con ta bai?")
+            case "Hoe heet je?": translate("Con jama bo?")
+            case "Mijn naam is...": translate("Mi nomber ta...")
+            case "Welkom!": translate("Bon bini!")
+            case "Ja": translate("Si")
+            case "Nee": translate("No")
+            case "Goed": translate("Bon")
+            case "Slecht": translate("Malo")
+            case "Matig": translate("Basta bon")
+            case "Goedendag": translate("Bondia")
+            case "Goedemiddag": translate("Bontardi")
+            case "Goedenavond": translate("Bonochi")
+            case "Welterusten": translate("Drumi dushi")
+            case "Tot morgen!": translate("Te majan!")
+            default: break
+        }
+    }
+    
+    func translate(word: String){
+        translated.text = word
+        print(word)
+    }
+    
+    
+    
 }
 
